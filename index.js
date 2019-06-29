7c5a5e6ae2e4dd7224e91b9bba5bb9dcd9d762c2
Kleyton Barcelos <kleytonbarcelos@gmail.com>
2019-06-28 23:31:50 -0400

    chore: Mudando a mensagem da rota

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  return res.json({ msg: "Hello World!!!" });
+  return res.json({ msg: "Hello World 2" });
 });
 
 app.listen(3000);
